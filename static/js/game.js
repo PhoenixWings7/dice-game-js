0025813686a58c0a8cbbe80cfa1811f20f0f7766
Klaudia Adamus <adamus.klaudia.pasp@gmail.com>
2020-01-08 22:26:59 +0100

    added comments, deleted some unused code

--- static/js/game.js
+++ static/js/game.js
@@ -5,8 +5,8 @@ const firstDice = document.getElementById('dice-1');
 const secondDice = document.getElementById('dice-2');
 
 
-//creating addToGlobalScore as a function and immiediately calling (invoking) it so it can become an inner function
-//because of closures
+//creating addToGlobalScore as a function and immediately calling (invoking) it so it can become an inner function
+//and use private variables stored in memory somewhere because of closures
 //addToGlobalScore takes arguments doAdd (true or false), playerNum, points
 let addToGlobalScore = (function () {
     let globalScore1 = 0;
@@ -32,6 +32,9 @@ let addToGlobalScore = (function () {
 }) ();
 
 
+//creating changeActivePlayer as a function and immediately calling (invoking) it so it can become an inner function
+//and use private variables stored in memory somewhere because of closures
+//changeActivePlayer takes args doChange, points (default=0) and returns active player if doChange=false or adds to score
 let changeActivePlayer = (function () {
     let activePlayerNum = 1;
     return function (doChange, points = 0) {
@@ -50,6 +53,9 @@ let changeActivePlayer = (function () {
 })();
 
 
+//creating addToCurrentScore as a function and immediately calling (invoking) it so it can become an inner function
+//and use private variables stored in memory somewhere because of closures
+//function takes args doAdd (adds to currentScore if true), resetCurrentScore (resets score if true) and points
 let addToCurrentScore = (function () {
     let currentScore = 0;
     return function (doAdd, resetCurrentScore, points=0) {
@@ -63,19 +69,21 @@ let addToCurrentScore = (function () {
     }
 })();
 
-let stand = (function () {
-    let currentScore = 0;
-    return function () {
-        let points = addToCurrentScore(false, false);
-        let activePlayer = changeActivePlayer(false);
-        changeActivePlayer(true, points);
-        addToCurrentScore(false, true);
-        let playerScore = addToGlobalScore(false, activePlayer, points);
-        let playerScoreDOM = document.getElementById(`total-score-${activePlayer}`);
-        playerScoreDOM.textContent = playerScore;
-    }
-})();
 
+//creating stand as a function to perform a few actions when user clicks 'stand' button
+//gets current points, adds them to global user score, resets them and changes score on the page
+function stand() {
+    let points = addToCurrentScore(false, false);
+    let activePlayer = changeActivePlayer(false);
+
+    changeActivePlayer(true, points);
+    addToCurrentScore(false, true);
+
+    //change global score on the page
+    let playerScore = addToGlobalScore(false, activePlayer, points);
+    let playerScoreDOM = document.getElementById(`total-score-${activePlayer}`);
+    playerScoreDOM.textContent = playerScore;
+}
 
 
 //event listeners
@@ -84,18 +92,20 @@ standBtn.addEventListener('click', stand);
 
 
 
-// functions
+//
 function rollDice() {
+    //define all needed variables or constants
     let activePlayerNum = changeActivePlayer(false);
     let currentScore = addToCurrentScore(false, false);
     const currentScoreDOM = document.getElementById(`round-score-${activePlayerNum}`);
     const dice1 = Math.floor(Math.random() * 6) + 1,
           dice2 = Math.floor(Math.random() * 6) + 1;
-    // console.log(currentScore.textContent);
     let additionalPoints = dice1 + dice2;
+
+    //change current score on the page and in current score private function
     currentScoreDOM.textContent = currentScore + additionalPoints;
     addToCurrentScore(true, false, additionalPoints);
+    //change dices' pictures
     firstDice.src = `../static/images/${String(dice1)}.png`;
     secondDice.src = `../static/images/${String(dice2)}.png`;
 }
-// activePlayerNum = changeActivePlayer(true, points);
