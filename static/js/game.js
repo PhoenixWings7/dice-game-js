d7e1807395a9d58da26cd78472d5686e7000d7b9
Klaudia Adamus <adamus.klaudia.pasp@gmail.com>
2020-01-08 16:00:37 +0100

    added private player-changing function

--- static/js/game.js
+++ static/js/game.js
@@ -2,9 +2,34 @@
 const rollBtn = document.getElementById('roll');
 const firstDice = document.getElementById('dice-1');
 const secondDice = document.getElementById('dice-2');
-let activePlayer ;
+let globalScore1;
+let globalScore2;
+
+let changeActivePlayer = (function () {
+    let activePlayerNum = 1;
+    return function (doChange) {
+        if (doChange) {
+            if (activePlayerNum === 1) {
+                activePlayerNum = 2;
+            }
+            else {
+                activePlayerNum = 1;
+            }
+
+        }
+        return activePlayerNum;
+    }
+})();
 
 
+//creating addToGlobalScore as a function and immiediately calling (invoking) it so it can become an inner function
+//because of closures
+let addToGlobalScore1 = (function () {
+    let globalScore1 = 0;
+    return function () {
+
+    }
+}) ();
 
 
 
@@ -18,15 +43,13 @@ rollBtn.addEventListener('click', rollDice);
 
 // functions
 function rollDice() {
-    console.log(activePlayer);
-    const currentScore = document.getElementById(`round-score-${activePlayer}`);
+    let activePlayerNum = changeActivePlayer(false);
+    const currentScore = document.getElementById(`round-score-${activePlayerNum}`);
     const dice1 = Math.floor(Math.random() * 6) + 1,
           dice2 = Math.floor(Math.random() * 6) + 1;
     // console.log(currentScore.textContent);
     currentScore.textContent = parseInt(currentScore.textContent, 10) + dice1 + dice2;
     firstDice.src = `../static/images/${String(dice1)}.png`;
     secondDice.src = `../static/images/${String(dice2)}.png`;
-}
-function gameInit() {
-    activePlayer = 1;
+    activePlayerNum = changeActivePlayer(true);
 }
